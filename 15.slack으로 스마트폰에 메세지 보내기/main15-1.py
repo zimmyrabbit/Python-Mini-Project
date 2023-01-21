4f8d4131430337314da09c0e9e75550d16d4e3a3
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-01-21 22:28:24 +0900

    slack으로 스마트폰에 메시지 보내기

---
+++ 15.slack으로 스마트폰에 메세지 보내기/main15-1.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+slack_webhook_url = "https://hooks.slack.com/services/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"
+
+def sendSlackWebhook(strText) :
+    headers = {
+        "Content-type": "application/json"
+    }
+    data = {
+        "text": strText
+    }
+
+    res = requests.post(slack_webhook_url, headers=headers, data=json.dumps(data))
+
+    if res.status_code == 200:
+        return "ok"
+    else:
+        return "error"
+
+print(sendSlackWebhook("파이썬에서 보내는 메세지 테스트"))
