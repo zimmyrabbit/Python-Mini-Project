56ed5841607b9f37d386fff724d43b13436c46d1
LAPTOP-UF7DLA8Q\zimmy <zimmyrabbit@naver.com>
2023-01-21 23:14:16 +0900

    받은 이메일 정보 가져오기

---
+++ 16.이메일로 특정 키워드가 오면 스마트폰으로 알림 보내기/main16-1.py
@@ -0,0 +1,38 @@
+from info import info
+import imaplib
+import email
+from email import policy
+
+def find_encoding_info(txt):
+    info = email.header.decode_header(txt)
+    subject, encode = info[0]
+    return subject, encode
+
+imap = imaplib.IMAP4_SSL('imap.naver.com')
+id = info.naver_id()
+pw = info.naver_pw()
+imap.login(id,pw)
+
+imap.select('INBOX')
+resp, data = imap.uid('search', None, 'All')
+all_email = data[0].split()
+last_email = all_email[-5:]
+
+for mail in reversed(last_email):
+    result, data = imap.uid('fetch', mail, '(RFC822)')
+    raw_email = data[0][1]
+    email_message = email.message_from_bytes(raw_email, policy=policy.default)
+
+    print('='*70)
+    print('FROM: ', email_message['From'])
+    print('SENDER: ', email_message['Sender'])
+    print('TO: ', email_message['To'])
+    print('DATE: ', email_message['Date'])
+    subject,encode = find_encoding_info(email_message['Subject'])
+    print('SUBJECT: ', subject)
+    print(encode)
+    print('='*70)
+
+imap.close()
+imap.logout()
+
---
+++ 16.이메일로 특정 키워드가 오면 스마트폰으로 알림 보내기/main16-2.py
@@ -0,0 +1,46 @@
+from info import info
+import imaplib
+import email
+from email import policy
+
+def find_encoding_info(txt):
+    info = email.header.decode_header(txt)
+    subject, encode = info[0]
+    return subject, encode
+
+imap = imaplib.IMAP4_SSL('imap.naver.com')
+id = info.naver_id()
+pw = info.naver_pw()
+imap.login(id,pw)
+
+imap.select('INBOX')
+resp, data = imap.uid('search', None, 'All')
+all_email = data[0].split()
+last_email = all_email[-5:]
+
+for mail in reversed(last_email):
+    result, data = imap.uid('fetch', mail, '(RFC822)')
+    raw_email = data[0][1]
+    email_message = email.message_from_bytes(raw_email, policy=policy.default)
+
+    print('='*70)
+    print('FROM: ', email_message['From'])
+    print('SENDER: ', email_message['Sender'])
+    print('TO: ', email_message['To'])
+    print('DATE: ', email_message['Date'])
+    subject,encode = find_encoding_info(email_message['Subject'])
+    print('SUBJECT: ', subject)
+    print('[CONTENT]')
+
+    message = ''
+    if email_message.is_multipart():
+        for part in email_message.get_payload():
+            if part.get_content_type() == 'text/plain':
+                bytes = part.get_payload(decode=True)
+                encode = part.get_content_charset()
+                message = message + str(bytes,encode)
+    print(message)
+    print('='*70)
+
+imap.close()
+imap.logout()
